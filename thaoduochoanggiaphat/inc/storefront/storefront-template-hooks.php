a8728d1fce97d431e9530227c650cf076caae8f9
thanhnhat <ngothanhnhat.it@gmail.com>
2018-07-22 15:34:22 +0700

    Customize Product Detail Page

--- thaoduochoanggiaphat/inc/storefront/storefront-template-hooks.php
+++ thaoduochoanggiaphat/inc/storefront/storefront-template-hooks.php
@@ -1,3 +1,5 @@
 <?php
 
-add_action( 'after_setup_theme', 'handle_parent_action', 10 );
+add_action( 'after_setup_theme', 'handle_parent_action', 20 );
+add_action( 'init', 'remove_parent_action_in_init', 100);
+
